9ca884de72f12d1e2ac7514eab9e9efda3cda380
sunyuhang <sunyuhang123256@pwrd.com>
2021-12-15 20:36:55 +0800

    fix：调整代码

--- 热题 HOT 100/3.无重复字符的最长子串/index.js
+++ 热题 HOT 100/3.无重复字符的最长子串/index.js
@@ -4,8 +4,8 @@ const subLen = str => {
   let max = 0;
 
   for (let i = 0; i < str.length; i++) {
-    if (arr.indexOf(str[i])) {
-      arr.slice(0, arr.indexOf(str[i]) + 1);
+    if (arr.indexOf(str[i]) !== -1) {
+      arr.splice(0, arr.indexOf(str[i]) + 1);
     }
     arr.push(str[i]);
     max = Math.max(max, arr.length);
